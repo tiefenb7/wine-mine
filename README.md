0c42f26a95e420ebd62e090ffe6ca9adfbde8206
tiefenb7 <tiefenb7@gmail.com>
2021-01-03 12:36:41 -0800

    Dynamically fetching search form to inject search key

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # wine-mine
 
-Run: 
-node screenshot.js https://www.google.com/
-node scrape.js https://www.wine.com/
+Run:\ 
+\
+node screenshot.js https://www.google.com/\
+\
+node scrape.js https://www.wine.com/ "1999"\
+node scrape.js https://en.wikipedia.org/ "chateau"\
+node scrape.js https://google.com/ "chateau margaux 1999"\
--- scrape.js
+++ scrape.js
@@ -1,21 +1,51 @@
 const puppeteer = require('puppeteer');
-const INPUT_TYPE_SEARCH = 'input[type=search]';
 
 /*
-    Method opens indexUrl and finds input of search type.
+    Method opens indexUrl and finds an input form.
     Then injects the searchKey and executes to result page.
 */
 async function searchSiteByKey(page, indexUrl, searchKey) {
     await page.goto(indexUrl);
+
+    const getData = async() => {
+        return await page.evaluate(async () => {
+            return await new Promise(resolve => {
+                let INPUT_TYPE_SEARCH = '';
+                let documentForm = document.forms[0];
+                for(var i = 0; i < documentForm.length; i++) {
+                    const inputTextExists = 
+                    documentForm[i].tagName &&
+                    documentForm[i].tagName.toLowerCase() == "input" && 
+                    documentForm[i].type.toLowerCase() == "text";
+        
+                    const inputSearchExists = 
+                    documentForm[i].tagName &&
+                    documentForm[i].tagName.toLowerCase() == "input" && 
+                    documentForm[i].type.toLowerCase() == "search";
+
+                    if(inputTextExists) {
+                        INPUT_TYPE_SEARCH = 'input[type=text]';
+                    } else if(inputSearchExists) {
+                        INPUT_TYPE_SEARCH = 'input[type=search]';
+                    } 
+                }
+                resolve(INPUT_TYPE_SEARCH);
+          })
+        })
+      }  
+
+
+    INPUT_TYPE_SEARCH = await getData();
+    await page.goto(indexUrl);
     await page.focus(INPUT_TYPE_SEARCH);
-    page.keyboard.type(searchKey);
+    await page.keyboard.type(searchKey);
     await page.waitForTimeout(3000); //not ideal. 
-    page.keyboard.press('Enter');
+    await page.keyboard.press('Enter');
 }
 
 async function run (indexUrl, searchKey) {
     //Launch browser with default options. 
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
     // Initiate search based on key
@@ -28,9 +58,6 @@ async function run (indexUrl, searchKey) {
     return 1;
 }
 
-let indexUrl = 'https://www.wine.com';
-let searchKey = '1999';
-
-run(indexUrl,searchKey).then(value => {
+run(process.argv[2],process.argv[3]).then(value => {
     console.log(value);
 });
